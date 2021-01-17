4416911e832dde5df4db1719cf44427dfc1278e1
S1mple <Z15225825811@163.com>
2021-01-17 20:56:09 +0800

    改假删

--- src/main/java/com/fh/shopapi/dao/DataValueDao.java
+++ src/main/java/com/fh/shopapi/dao/DataValueDao.java
@@ -9,7 +9,7 @@ import org.apache.ibatis.annotations.Update;
 import java.util.List;
 
 public interface DataValueDao {
-    @Select("select * from t_datavalue where attId = #{attId}")
+    @Select("select * from t_datavalue where attId = #{attId} and isDel = 0")
     List<DataValue> getDataByAttId(Integer attId);
     @Insert("insert into t_datavalue (value,valueCH,attId) value (#{value},#{valueCH},#{attId})")
     void add(DataValue dataValue);
@@ -17,6 +17,6 @@ public interface DataValueDao {
     DataValue getDataById(Integer id);
     @Update("update t_datavalue set value=#{value},valueCH=#{valueCH} where id=#{id} ")
     void update(DataValue dataValue);
-    @Delete("delete from t_datavalue where id =#{id}")
+    @Delete("update  t_datavalue isDel = 1 where id =#{id}")
     void del(Integer id);
 }
--- src/main/java/com/fh/shopapi/entity/po/DataValue.java
+++ src/main/java/com/fh/shopapi/entity/po/DataValue.java
@@ -5,6 +5,15 @@ public class DataValue {
     private  String value;
     private  String valueCH;
     private  Integer attId;
+    private  Integer isDel;
+
+    public Integer getIsDel() {
+        return isDel;
+    }
+
+    public void setIsDel(Integer isDel) {
+        this.isDel = isDel;
+    }
 
     public Integer getId() {
         return id;
